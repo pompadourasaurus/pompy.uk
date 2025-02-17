5b1abd1c9354fb6c254c7e800dae62bf6a080944
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 17:41:56 +0000

    remove unused `fixed`

--- client/src/app/recipes/layout.tsx
+++ client/src/app/recipes/layout.tsx
@@ -18,7 +18,7 @@ export default function RecipesLayout({
           "lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10",
         )}
       >
-        <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 border-r md:sticky md:block">
+        <aside className="top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 border-r md:sticky md:block">
           <div className="no-scrollbar h-full overflow-auto py-6 pr-4 lg:py-8">
             <RecipesNav config={recipesNavConfig} />
           </div>
