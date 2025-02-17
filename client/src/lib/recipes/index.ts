1312a080f65d8c72c954609e7c43103d1a6398e9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 13:25:50 +0000

    little refactor for `getAllRecipes`

--- client/src/lib/recipes/index.ts
+++ client/src/lib/recipes/index.ts
@@ -1,12 +1,21 @@
-import type { Recipe } from "@/lib/types/recipe"
-
 import { myFirstRecipe } from "@/content/recipes/my-first-recipe"
+
+import type { RecipeTypeSlug } from "@/lib/recipes/recipe-types"
+import type { Recipe } from "@/lib/types/recipe"
 
 const recipes = [myFirstRecipe] as const satisfies Recipe[]
 
 export type RecipeSlug = (typeof recipes)[number]["slug"]
 
 const recipesMap = new Map<string, Recipe>(recipes.map((recipe) => [recipe.slug, recipe]))
+
+/**
+ * This comparison function orders recipes from most recent to least recent.
+ * In other words, it sorts recipes such that their dates are in descending order.
+ */
+function compareRecipesByDate(first: Recipe, second: Recipe): number {
+  return +second.date - +first.date
+}
 
 export function getRecipeSlugs(): RecipeSlug[] {
   return Array.from(recipesMap.keys()) as RecipeSlug[]
@@ -18,6 +27,6 @@ export function getRecipeBySlug(slug: string) {
   return recipesMap.get(slug)
 }
 
-export function getAllRecipes(): (typeof recipes)[number][] {
-  return recipes.toSorted((first, second) => (first.date > second.date ? -1 : 1))
+export function getAllRecipes(): readonly (typeof recipes)[number][] {
+  return recipes.toSorted(compareRecipesByDate)
 }
