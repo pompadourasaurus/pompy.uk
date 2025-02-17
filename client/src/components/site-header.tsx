606fafee354f1ecca11ae12667c3089b4d6af710
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 17:41:37 +0000

    remove `border-grid`

--- client/src/components/site-header.tsx
+++ client/src/components/site-header.tsx
@@ -24,7 +24,7 @@ function GitHubLink() {
 
 export function SiteHeader({ navConfig }: { navConfig: RecipesNavConfig }) {
   return (
-    <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container-wrapper">
         <div className="container flex h-14 items-center">
           {/* Left-aligned nav + mobile nav */}
