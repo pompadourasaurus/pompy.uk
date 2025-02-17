912871ac8614a9ee2bcc32e984318e9042ab058c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 11:17:28 +0000

    remove unused remote image source

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.discordapp.com",
-      },
-    ],
-  },
-}
+const nextConfig: NextConfig = {}
 
 class ConfigBuilder {
   config: NextConfig
