a0d5146e4e80c76a8309cd010cf062ac4541d470
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 11:27:34 +0000

    swap `Blog.` to `Pompy.`

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
       <Link href="/" className="hover:underline">
-        Blog
+        Pompy
       </Link>
       .
     </h2>
