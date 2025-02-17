67ed52965b5ec9d05046bd21adfc9a5a00bbc608
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-02-17 15:08:23 +0000

    feat: add `ModeSwitcher` component

---
+++ client/src/components/mode-switcher.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { MoonIcon, SunIcon } from "lucide-react"
+import { useTheme } from "next-themes"
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import { metaThemeColours } from "@/config/site"
+import { useMetaColour } from "@/hooks/use-meta-colour"
+
+export function ModeSwitcher() {
+  const { setTheme, resolvedTheme } = useTheme()
+  const { setMetaColor } = useMetaColour()
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark")
+    setMetaColor(resolvedTheme === "dark" ? metaThemeColours.light : metaThemeColours.dark)
+  }, [resolvedTheme, setTheme, setMetaColor])
+
+  return (
+    <Button variant="ghost" className="group/toggle h-8 w-8 px-0" onClick={toggleTheme}>
+      <SunIcon className="hidden [html.dark_&]:block" />
+      <MoonIcon className="hidden [html.light_&]:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
