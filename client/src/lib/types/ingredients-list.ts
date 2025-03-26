d1dbabf8c4832dd637835f2f08398cb7bc24b194
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-26 20:56:52 +0000

    add `IngredientsList` type

---
+++ client/src/lib/types/ingredients-list.ts
@@ -0,0 +1,9 @@
+import type { IngredientSlug } from "@/lib/ingredients"
+import type { Quantity } from "@/lib/types/quantity"
+
+type IngredientListItem = {
+  ingredient: IngredientSlug
+  detail?: string | null | undefined
+} & Quantity
+
+export type IngredientsList = IngredientListItem[]
