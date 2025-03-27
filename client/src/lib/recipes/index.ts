6df6a504da6637e07233e0ecd2fe4462ec3449b9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-27 02:15:54 +0000

    remove placeholder recipes

--- client/src/lib/recipes/index.ts
+++ client/src/lib/recipes/index.ts
@@ -1,12 +1,9 @@
-import myFirstRecipe from "@/content/recipes/my-first-recipe"
-import mySecondRecipe from "@/content/recipes/my-second-recipe"
-import myThirdRecipe from "@/content/recipes/my-third-recipe"
 import whiteChocolateAndRaspberryCookies from "@/content/recipes/white-chocolate-and-raspberry-cookies"
 
 import type { RecipeTypeSlug } from "@/lib/recipes/recipe-types"
 import type { Recipe, RecipeSlug } from "@/lib/types/recipe"
 
-const recipes = [myFirstRecipe, mySecondRecipe, myThirdRecipe, whiteChocolateAndRaspberryCookies] satisfies Recipe[]
+const recipes = [whiteChocolateAndRaspberryCookies] satisfies Recipe[]
 
 const recipesMap = new Map<string, Recipe>(recipes.map((recipe) => [recipe.slug, recipe]))
 
