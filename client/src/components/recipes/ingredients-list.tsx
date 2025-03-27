48d8a75cd519b636d6de55033b2ba0372a525b57
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-27 02:07:19 +0000

    move utility functions related to quantities

--- client/src/components/recipes/ingredients-list.tsx
+++ client/src/components/recipes/ingredients-list.tsx
@@ -2,64 +2,14 @@
 
 import { useRecipeContext } from "@/components/recipes/recipe-context"
 import { getIngredientBySlug } from "@/lib/ingredients"
-import { getUnitBySlug } from "@/lib/quantities/units"
-import { isNumber } from "@/lib/type-guards"
-import type { Fraction } from "@/lib/types/fraction"
+import { isPlural, renderQuantity } from "@/lib/quantities"
 import type { Ingredient } from "@/lib/types/ingredient"
 import type { IngredientsList as IngredientsListType } from "@/lib/types/ingredients-list"
-import type {
-  Quantity,
-  QuantityRange,
-  SpecialQuantityRange,
-  SpecialSpecificQuantity,
-  SpecificQuantity,
-} from "@/lib/types/quantity"
-
-function renderAmount(amount: number | Fraction): string {
-  if (isNumber(amount)) return `${amount}`
-  // todo: fancy fraction rendering, anyone?
-  return `${amount.numerator}/${amount.denominator}`
-}
-
-function renderOrdinaryQuantity(quantity: SpecificQuantity | QuantityRange): string {
-  const unit = getUnitBySlug(quantity.unit)
-
-  if (quantity.quantityType === "range") {
-    const unitString = unit.label.symbol ?? unit.label.plural
-    return `${renderAmount(quantity.fromAmount)}-${renderAmount(quantity.toAmount)}${unitString}`
-  }
-
-  const isPlural = quantity.amount !== 1
-  const unitString = unit.label.symbol ?? (isPlural ? unit.label.plural : unit.label.singular)
-  return `${renderAmount(quantity.amount)}${unitString}`
-}
-
-function renderSpecialQuantity(quantity: SpecialSpecificQuantity | SpecialQuantityRange): string {
-  if (quantity.quantityType === "special-range") {
-    return `${renderAmount(quantity.fromAmount)}-${renderAmount(quantity.toAmount)} ${quantity.label}`
-  }
-
-  return `${renderAmount(quantity.amount)} ${quantity}`
-}
-
-function renderQuantity(quantity: Quantity): string {
-  if (quantity.quantityType == null || quantity.quantityType === "specific" || quantity.quantityType === "range")
-    return renderOrdinaryQuantity(quantity)
-
-  if (quantity.quantityType === "special-specific" || quantity.quantityType === "special-range")
-    return renderSpecialQuantity(quantity)
-
-  throw new Error(`tried to render quantity with unknown quantity type ${quantity.quantityType}`)
-}
 
 function renderIngredient(ingredient: Ingredient, plural: boolean): string {
   if (ingredient.label.countable === false) return ingredient.label.noun
   if (plural) return ingredient.label.plural
   return ingredient.label.singular
-}
-
-function isPlural(quantity: Quantity): boolean {
-  return quantity.quantityType === "range" || quantity.quantityType === "special-range" || quantity.amount !== 1
 }
 
 function IngredientsListItem({ listItem }: { listItem: IngredientsListType[number] }) {
---
+++ client/src/lib/quantities/index.ts
@@ -0,0 +1,81 @@
+import { getUnitBySlug } from "@/lib/quantities/units"
+import { isNumber } from "@/lib/type-guards"
+import type { Fraction } from "@/lib/types/fraction"
+import type {
+  Quantity,
+  QuantityRange,
+  SpecialQuantityRange,
+  SpecialSpecificQuantity,
+  SpecificQuantity,
+} from "@/lib/types/quantity"
+
+const unicodeFractions = new Map<string, string>([
+  ["1/2", "½"],
+  ["1/3", "⅓"],
+  ["2/3", "⅔"],
+  ["1/4", "¼"],
+  ["3/4", "¾"],
+  ["1/5", "⅕"],
+  ["2/5", "⅖"],
+  ["3/5", "⅗"],
+  ["4/5", "⅘"],
+  ["1/6", "⅙"],
+  ["5/6", "⅚"],
+  ["1/7", "⅐"],
+  ["1/8", "⅛"],
+  ["3/8", "⅜"],
+  ["5/8", "⅝"],
+  ["7/8", "⅞"],
+  ["1/9", "⅑"],
+  ["1/10", "⅒"],
+])
+
+function renderAmount(amount: number | Fraction): string {
+  if (isNumber(amount)) return `${amount}`
+  const quotient = (amount.numerator / amount.denominator) | 0 // bitwise OR implicitly 'casts' to integer
+  const remainder = amount.numerator % amount.denominator
+  if (remainder === 0) return `${quotient}`
+  let remainderString = `${remainder}/${amount.denominator}`
+  remainderString = unicodeFractions.get(remainderString) ?? remainderString
+  if (quotient === 0) return remainderString
+  return `${quotient} ${remainderString}`
+}
+
+function renderOrdinaryQuantity(quantity: SpecificQuantity | QuantityRange): string {
+  const unit = getUnitBySlug(quantity.unit)
+
+  if (quantity.quantityType === "range") {
+    const unitString = unit.label.symbol ?? ` ${unit.label.plural}`
+    const amountString = `${renderAmount(quantity.fromAmount)}-${renderAmount(quantity.toAmount)}`
+    if (unitString.trim().length === 0) return amountString
+    return `${amountString}${unitString}`
+  }
+
+  const isPlural = quantity.amount !== 1
+  const unitString = unit.label.symbol ?? (isPlural ? ` ${unit.label.plural}` : ` ${unit.label.singular}`)
+  const amountString = renderAmount(quantity.amount)
+  if (unitString.trim().length === 0) return amountString
+  return `${amountString}${unitString}`
+}
+
+function renderSpecialQuantity(quantity: SpecialSpecificQuantity | SpecialQuantityRange): string {
+  if (quantity.quantityType === "special-range") {
+    return `${renderAmount(quantity.fromAmount)}-${renderAmount(quantity.toAmount)} ${quantity.label}`
+  }
+
+  return `${renderAmount(quantity.amount)} ${quantity}`
+}
+
+export function renderQuantity(quantity: Quantity): string {
+  if (quantity.quantityType == null || quantity.quantityType === "specific" || quantity.quantityType === "range")
+    return renderOrdinaryQuantity(quantity)
+
+  if (quantity.quantityType === "special-specific" || quantity.quantityType === "special-range")
+    return renderSpecialQuantity(quantity)
+
+  throw new Error(`tried to render quantity with unknown quantity type ${quantity.quantityType}`)
+}
+
+export function isPlural(quantity: Quantity): boolean {
+  return quantity.quantityType === "range" || quantity.quantityType === "special-range" || quantity.amount !== 1
+}
