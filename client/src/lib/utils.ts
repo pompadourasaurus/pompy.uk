58d594dc578f77a71174f664955c801863c99cc0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-01 00:53:53 +0100

    add some extra utility types + `optionsMerge` function for the fun of it

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -10,3 +10,15 @@ export function cn(...inputs: ClassValue[]) {
 export function fraction(a: number, b: number): Fraction {
   return { numerator: a, denominator: b }
 }
+
+export function optionsMerge<TInput extends Record<string, unknown>, TResolved extends TInput>(
+  options: TInput | null | undefined,
+  defaults: TResolved,
+): Readonly<TResolved> {
+  if (options == null) return defaults
+  const resolvedOptions: Record<string, unknown> = Object.assign({}, options)
+  for (const [key, value] of Object.entries(defaults)) {
+    resolvedOptions[key] ??= value
+  }
+  return resolvedOptions as TResolved
+}
