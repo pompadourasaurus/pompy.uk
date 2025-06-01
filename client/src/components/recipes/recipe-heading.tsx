b525ed8ffb6f7e5e307d0de109dd93a075b91979
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-01 20:35:45 +0100

    prefer `ComponentProps` to `HTMLAttributes` in `RecipeHeading`

--- client/src/components/recipes/recipe-heading.tsx
+++ client/src/components/recipes/recipe-heading.tsx
@@ -2,10 +2,6 @@ import type * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export function RecipeHeading({ children, className, ...props }: React.HTMLAttributes<HTMLHeadElement>) {
-  return (
-    <h2 className={cn("text-xl font-semibold pb-4 border-b mt-10 mb-4", className)} {...props}>
-      {children}
-    </h2>
-  )
+export function RecipeHeading({ className, ...props }: React.ComponentProps<"h2">) {
+  return <h2 className={cn("text-xl font-semibold pb-4 border-b mt-10 mb-4", className)} {...props} />
 }
